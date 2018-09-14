479862c9a41559c2dccacf02fdf610f77fe55c8c
Flecheck <bessou.mathieu@gmail.com>
2018-09-14 20:49:02 +0200

    waves

--- src/waves.rs
+++ src/waves.rs
@@ -5,6 +5,7 @@ use ncollide3d::query::Ray;
 use ncollide3d::shape::ShapeHandle;
 use rayon::prelude;
 use std;
+use std::thread;
 
 const NB_SAMPLE: u32 = 100;
 const NB_SAMPLEF: f32 = NB_SAMPLE as f32;
@@ -14,15 +15,34 @@ const PI: f32 = std::f32::consts::PI;
 pub type EnergyRay = (Ray<f32>, f32, f32);
 
 pub fn tracing(world: &mut WorldDescriptor) {
-    let (sender, receiver) = channel::unbounded();
+    let (s, r) = channel::unbounded();
+    let (so, ro) = channel::unbounded();
+
     for x in &world.emitters {
         let rays = emit(&x.position).map(|ray| (ray, x.max_power, 0.));
         for ray in rays {
-            find_receiver(&ray);
-            for ray in next_rays(&ray) {
-                sender.send(ray);
-            }
+            s.send(ray);
         }
+    }
+
+    for _ in 0..4 {
+        let r = r.clone();
+        let s = s.clone();
+        let so = so.clone();
+        thread::spawn(move || {
+            for ray in r {
+                let c = find_receiver(&ray);
+                so.send(c);
+                for ray in next_rays(&ray) {
+                    s.send(ray);
+                }
+            }
+        });
+    }
+    drop(s);
+
+    for (ide, idr, pow, dist) in ro {
+        world.emitters[ide]
     }
 }
 
@@ -37,8 +57,10 @@ fn emit<'a>(pos: &'a Point3<f32>) -> impl Iterator<Item = Ray<f32>> + 'a {
     })
 }
 
-fn find_receiver(ray: &EnergyRay) {}
+fn find_receiver(ray: &EnergyRay) {
+    unimplemented!()
+}
 
-fn next_rays(ray: &EnergyRay) -> Vec<Ray<f32>> {
+fn next_rays(ray: &EnergyRay) -> Vec<EnergyRay> {
     unimplemented!()
 }
