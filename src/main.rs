71914b07361ea8a26485e75cdcf00d8a90fc6ade
Théo Degioanni <moxinilian@tutanota.com>
2018-09-14 17:59:40 +0200

    camel

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ extern crate ncollide3d;
 
 mod antennas;
 
-pub type float = f32;
+pub type Float = f32;
 
-pub const WAVE_VELOCITY: float = 299_792_458.; // meters per second
-pub const MAX_FREQUENCY: float = 60_000_000_000.;
-pub const TIME_PER_BEAT: float = 1. / MAX_FREQUENCY; // seconds
+pub const WAVE_VELOCITY: Float = 299_792_458.; // meters per second
+pub const MAX_FREQUENCY: Float = 60_000_000_000.;
+pub const TIME_PER_BEAT: Float = 1. / MAX_FREQUENCY; // seconds
 
 fn main() {
     println!("Hello, world!");
