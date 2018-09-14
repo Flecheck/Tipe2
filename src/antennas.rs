8c9e6d08a1471fdb9a9c1a42cb550cfa6e1bcd3c
Théo Degioanni <moxinilian@tutanota.com>
2018-09-14 18:07:59 +0200

    bouged

--- src/antennas.rs
+++ src/antennas.rs
@@ -4,17 +4,17 @@ use ncollide3d::shape::ShapeHandle;
 pub struct SignalReceiver {
     position: Point3<f32>,
     max_power: f32,
-
-    transfers: Vec<Vec<(u32, f32)>>, // indexed by emitter, (time, gain)
 }
 
 pub struct SignalEmitter {
     position: Point3<f32>,
     max_power: f32,
+
+    transfers: Vec<Vec<(u32, f32)>>, // indexed by emitter, (time, gain)
 }
 
 pub struct WorldDescriptor {
     emitters: Vec<SignalEmitter>,
     receivers: Vec<SignalReceiver>,
-    //collisions: Vec<ShapeHandle>,
+    collisions: Vec<ShapeHandle<f32>>,
 }
