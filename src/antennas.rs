cbc862cca3f38b4ddcb70fe25562ea3be5e75d15
Théo Degioanni <moxinilian@tutanota.com>
2018-09-14 17:58:38 +0200

    stuff

--- src/antennas.rs
+++ src/antennas.rs
@@ -1,4 +1,20 @@
+use nalgebra::Point3;
+use ncollide3d::shape::ShapeHandle;
 
 pub struct SignalReceiver {
-    
+    position: Point3<f32>,
+    max_power: f32,
+
+    transfers: Vec<Vec<(u32, f32)>>, // indexed by emitter, (time, gain)
+}
+
+pub struct SignalEmitter {
+    position: Point3<f32>,
+    max_power: f32,
+}
+
+pub struct WorldDescriptor {
+    emitters: Vec<SignalEmitter>,
+    receivers: Vec<SignalReceiver>,
+    //collisions: Vec<ShapeHandle>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #![feature(nll)]
 
 extern crate nalgebra;
+extern crate ncollide3d;
 
 mod antennas;
+
+pub type float = f32;
+
+pub const WAVE_VELOCITY: float = 299_792_458.; // meters per second
+pub const MAX_FREQUENCY: float = 60_000_000_000.;
+pub const TIME_PER_BEAT: float = 1. / MAX_FREQUENCY; // seconds
 
 fn main() {
     println!("Hello, world!");
